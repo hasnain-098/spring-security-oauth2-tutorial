ba74cb8102caea210323952e39b419f6422a1dd2
Hasnain Memon <hasnainmemon.018@gmail.com>
2026-01-16 20:43:28 +0500

    chore: load GitHub OAuth2 client secret from environment variable

--- src/main/java/dev/hasnain/oauth_tutorial/Application.java
+++ src/main/java/dev/hasnain/oauth_tutorial/Application.java
@@ -1,5 +1,6 @@
 package dev.hasnain.oauth_tutorial;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("CLIENT_SECRET", dotenv.get("CLIENT_SECRET"));
+		System.out.println(dotenv.get("CLIENT_SECRET"));
+
 		SpringApplication.run(Application.class, args);
 	}
 
