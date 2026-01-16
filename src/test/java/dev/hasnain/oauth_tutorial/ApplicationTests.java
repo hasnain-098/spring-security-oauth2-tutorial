bf6ce282de42374a404425bc0cf0212c862b544f
Hasnain Memon <hasnainmemon.018@gmail.com>
2026-01-16 17:16:05 +0500

    Initial commit

---
+++ src/test/java/dev/hasnain/oauth_tutorial/ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hasnain.oauth_tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
